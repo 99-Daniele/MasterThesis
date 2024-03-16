7a987d41186ba8c4524483ebe161f5fff4bdcf61
Daniele Cicala <10630561@polimi.it>
2024-03-16 18:17:28 +0100

    moved import to make faster

--- main.py
+++ main.py
@@ -1,83 +1,100 @@
-import dash as ds
-
-import utils.DatabaseConnection as connect
-import utils.DataUpdate as update
-import utils.Getters as getter
-import utils.Graph.ComparationGraph as comparation
-import utils.Graph.DurationGraph as duration
-import utils.Graph.EventsGraph as event
-import utils.Utilities as utilities
-
-import pages.Home as homePage
-import pages.Comparation as comparationPage
-import pages.Duration as durationPage
-import pages.Event as eventPage
-import pages.ComparationGraph.ComparationByMonth as comparationMonthPage
-import pages.ComparationGraph.ComparationByMonthYear as comparationMonthYearPage
-import pages.ComparationGraph.ComparationByWeek as comparationWeekPage
-import pages.DurationGraph.PhaseDuration as phaseDurationPage
-import pages.DurationGraph.CourtHearingDuration as courtHearingDurationPage
-import pages.DurationGraph.EventDuration as eventDurationPage
-import pages.DurationGraph.PhaseDuration as phaseDurationPage
-import pages.DurationGraph.ProcessDuration as processDurationPage
-import pages.DurationGraph.StateDuration as stateDurationPage
-import pages.EventGraph.AllEvents as allEventsPage
-import pages.EventGraph.CourtHearingEvents as courtHearingEventsPage
-import pages.EventGraph.ImportantEvents as importantEventsPage
-
 def refreshData():
+    import utils.DatabaseConnection as connect
+    import utils.DataUpdate as update
+    import utils.Utilities as utilities
     databaseInfo = utilities.dataBaseInfo
     connection = connect.connectToDatabase(databaseInfo[0], databaseInfo[1], databaseInfo[2], databaseInfo[3])
     update.refreshData(connection)
 
 def displayAllEvents():
+    import utils.Getters as getter
+    import utils.Graph.EventsGraph as event
     events = getter.getAllEvents()
     importantEventsType = getter.getImportantEventsType()
     event.displayEvents(events, importantEventsType)
 
 def displayImportantEvents():
+    import utils.Getters as getter
+    import utils.Graph.EventsGraph as event
     importantEvents = getter.getImportantEvents()
     importantEventsType = getter.getImportantEventsType()
     event.displayEvents(importantEvents, importantEventsType)
 
 def displayCourtHearingEvents():
+    import utils.Getters as getter
+    import utils.Graph.EventsGraph as event
     courtHearingEvents = getter.getCourtHearingEvents()
     courtHearingEventsType = getter.getCourtHearingEventsType()
     event.displayEvents(courtHearingEvents, courtHearingEventsType)
 
 def displayProcessesDuration():
+    import utils.Getters as getter
+    import utils.Graph.DurationGraph as duration
     processes = getter.getProcessesDuration()
     duration.displayProcessesDuration(processes)
 
 def displayStatesDuration():
+    import utils.Getters as getter
+    import utils.Graph.DurationGraph as duration
     states = getter.getStatesDuration()
     duration.displayStatesDuration(states)
 
 def displayPhasesDuration():
+    import utils.Getters as getter
+    import utils.Graph.DurationGraph as duration
     phases = getter.getPhasesDuration()
     duration.displayPhasesDuration(phases)
 
 def displayEventsDuration():
+    import utils.Getters as getter
+    import utils.Graph.DurationGraph as duration
     events = getter.getEventsDuration()
     duration.displayEventsDuration(events)
     
 def displayCourtHearingsDuration():
+    import utils.Getters as getter
+    import utils.Graph.DurationGraph as duration
     courtHearings = getter.getCourtHearingsDuration()
     duration.displayCourtHearingsDuration(courtHearings)
 
 def displayComparationByWeek():
+    import utils.Getters as getter
+    import utils.Graph.ComparationGraph as comparation
     processes = getter.getProcessesDuration()
     comparation.displayComparation(processes, "W")
 
 def displayComparationByMonth():
+    import utils.Getters as getter
+    import utils.Graph.ComparationGraph as comparation
     processes = getter.getProcessesDuration()
     comparation.displayComparation(processes, "M")
 
 def displayComparationByMonthYear():
+    import utils.Getters as getter
+    import utils.Graph.ComparationGraph as comparation
     processes = getter.getProcessesDuration()
     comparation.displayComparation(processes, "MY")
 
 def startApp():
+    import dash as ds
+
+    import pages.Home as homePage
+    import pages.Comparation as comparationPage
+    import pages.Duration as durationPage
+    import pages.Event as eventPage
+    import pages.ComparationGraph.ComparationByMonth as comparationMonthPage
+    import pages.ComparationGraph.ComparationByMonthYear as comparationMonthYearPage
+    import pages.ComparationGraph.ComparationByWeek as comparationWeekPage
+    import pages.DurationGraph.PhaseDuration as phaseDurationPage
+    import pages.DurationGraph.CourtHearingDuration as courtHearingDurationPage
+    import pages.DurationGraph.EventDuration as eventDurationPage
+    import pages.DurationGraph.PhaseDuration as phaseDurationPage
+    import pages.DurationGraph.ProcessDuration as processDurationPage
+    import pages.DurationGraph.StateDuration as stateDurationPage
+    import pages.EventGraph.AllEvents as allEventsPage
+    import pages.EventGraph.CourtHearingEvents as courtHearingEventsPage
+    import pages.EventGraph.ImportantEvents as importantEventsPage
+
     app = ds.Dash(__name__, suppress_callback_exceptions = True)
     app.layout = ds.html.Div([
         ds.html.H2('PROGETTO TRIBUNALI'),
@@ -122,4 +139,4 @@ def startApp():
     app.run_server(debug = True)
 
 if __name__ == '__main__':
-    startApp()
+    displayProcessesDuration()
