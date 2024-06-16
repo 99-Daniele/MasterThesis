40412c3340f44066426b2820a4ea7665ee2d20f3
Daniele Cicala <10630561@polimi.it>
2024-06-16 15:25:40 +0200

    new predictor?

--- utils/Prediction.py
+++ utils/Prediction.py
@@ -17,6 +17,73 @@ def predictDurationsWithoutLikenessTest(df, codeJudgeTag, codeSubjectTag, countT
     columns.remove(finishedTag)
     columns.remove(durationFinalTag)
     numProcesses = frame.getUniques(df, numProcessTag).tolist()
+    lTrain = int(len(numProcesses) / 5)
+    trainNumProcesses = numProcesses[lTrain:]
+    testNumProcesses = list(set(numProcesses) - set(trainNumProcesses))
+    errors = []
+    predictions = []
+    with alive_bar(int(len(numProcesses))) as bar:
+        for i in range(int(len(numProcesses))):
+            testDF = df[df[numProcessTag].isin(testNumProcesses)].copy()
+            lTest = len(testDF)
+            r = rd.randint(0, lTest - 1)
+            testDF_temp = testDF.iloc[r]
+            judge = testDF_temp[codeJudgeTag]
+            subject = testDF_temp[codeSubjectTag]
+            section = testDF_temp[sectionTag]
+            processID = testDF_temp[numProcessTag]
+            count = testDF_temp[countTag]
+            currDuration = testDF_temp[durationTag]
+            trainDF_temp = df[df[numProcessTag].isin(trainNumProcesses)].copy()
+            if len(trainDF_temp[trainDF_temp[sectionTag] == section]) > 0:
+                trainDF_temp = trainDF_temp[trainDF_temp[sectionTag] == section]
+            if len(trainDF_temp[trainDF_temp[codeJudgeTag] == judge]) > 0:
+                trainDF_temp = trainDF_temp[trainDF_temp[codeJudgeTag] == judge]
+            if len(trainDF_temp[trainDF_temp[codeSubjectTag] == subject]) > 0:
+                trainDF_temp = trainDF_temp[trainDF_temp[codeSubjectTag] == subject]
+            lenTrainTemp = len(trainDF_temp)
+            if lenTrainTemp > 0:
+                trainX = trainDF_temp[columns]
+                trainY = trainDF_temp[[durationFinalTag]]
+                testX = testDF_temp[columns]
+                testY = testDF_temp[[durationFinalTag]]
+                model  = DecisionTreeClassifier()
+                model.fit(trainX.values, trainY)
+                predictedDuration = model.predict([testX.values])[0]
+                currDuration = testX[durationTag]
+                finalDuration = currDuration + testY[durationFinalTag]
+                predictedFinalDuration = currDuration + predictedDuration 
+                if finalDuration > 0:
+                    error = abs(predictedFinalDuration - finalDuration) * 100 / finalDuration
+                    errors.extend([error])
+                    predictions.extend([{numProcessTag: str(processID), countTag: str(count), durationTag: str(currDuration), durationFinalTag: str(finalDuration), durationPredictedTag: str(predictedFinalDuration), 'errore': error}])
+            bar() 
+    errors = sorted(errors)
+    m = int(len(errors) / 2)
+    if len(errors) % 2 != 0:
+        medianError = errors[m]
+    else:
+        medianError = (errors[m - 1] + errors[m]) / 2.0
+    meanError = sum(errors) / len(errors)
+    predictions = sorted(predictions, key = lambda x: x['errore'])
+    file.writeOnJsonFile('cache/predictions.json', predictions)
+    #plt.scatter(testY, predictedDurations)
+    #plt.xlabel('Actual Petal Width')
+    #plt.ylabel('Predicted Petal Width')
+    #plt.title('Actual vs Predicted Petal Width')
+    #plt.show()
+    return meanError, medianError
+
+# predict duration of finished processes based on current events flow. This evaluate the error of the model.
+def predictDurationsWithoutLikenessTest2(df, codeJudgeTag, codeSubjectTag, countTag, durationTag, durationFinalTag, durationPredictedTag, finishedTag, numProcessTag, sectionTag):
+    columns = df.columns.values.tolist()
+    columns.remove(codeJudgeTag)
+    columns.remove(codeSubjectTag)
+    columns.remove(sectionTag)
+    columns.remove(numProcessTag)
+    columns.remove(finishedTag)
+    columns.remove(durationFinalTag)
+    numProcesses = frame.getUniques(df, numProcessTag).tolist()
     errors = []
     predictions = []
     with alive_bar(int(len(numProcesses) / 20)) as bar:
